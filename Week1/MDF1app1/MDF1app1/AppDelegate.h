1939da836c0c1ab813dd39403e45cde94955fe11
Courtney Ardis <ardiscb@fullsail.edu>
2013-04-08 18:18:45 -0400

    Added initial files for project 1.

---
+++ Week1/MDF1app1/MDF1app1/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MDF1app1
+//
+//  Created by Courtney Ardis on 4/8/13.
+//  Copyright (c) 2013 Courtney Ardis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Week1/MDF1app1/MDF1app1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MDF1app1
+//
+//  Created by Courtney Ardis on 4/8/13.
+//  Copyright (c) 2013 Courtney Ardis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
