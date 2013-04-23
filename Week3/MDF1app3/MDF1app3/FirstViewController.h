a1bea86aef8bdb220af584c3a13ff0b38f0385fc
Courtney Ardis <ardiscb@fullsail.edu>
2013-04-23 19:53:33 -0400

    Added initial files for Project 3.

---
+++ Week3/MDF1app3/MDF1app3/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  MDF1app3
+//
+//  Created by Courtney Ardis on 4/23/13.
+//  Copyright (c) 2013 Courtney Ardis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ Week3/MDF1app3/MDF1app3/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  MDF1app3
+//
+//  Created by Courtney Ardis on 4/23/13.
+//  Copyright (c) 2013 Courtney Ardis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
